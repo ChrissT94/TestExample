04054f0ac81b8716c0bf64abb41bfc832f9d67a0
ChrissT94 <cristiantorresdam@gmail.com>
2020-03-02 13:07:31 -0500

    first commit

---
+++ src/main/java/org/chriss/Test/App.java
@@ -0,0 +1,13 @@
+package org.chriss.Test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
